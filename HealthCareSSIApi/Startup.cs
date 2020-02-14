68be69e1d7e58a082efb482f16ce045bf4a57801
jbrown <jordan@brown.com>
2020-02-14 11:48:15 -0700

    Deployment in AWS EBS working.

--- HealthCareSSIApi/Startup.cs
+++ HealthCareSSIApi/Startup.cs
@@ -8,17 +8,69 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
 
 namespace HealthCareSSIApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{GetEnvironmentName(env)}.json", optional: true)
+                .AddJsonFile(@"C:\Program Files\Amazon\ElasticBeanstalk\config\containerconfiguration", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            builder.AddInMemoryCollection(ParseEbConfig(config));
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
+
+        private static Dictionary<string, string> ParseEbConfig(IConfiguration config)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            foreach (IConfigurationSection pair in config.GetSection("iis:env").GetChildren())
+            {
+                string[] keypair = pair.Value.Split(new[] { '=' }, 2);
+                dict.Add(keypair[0], keypair[1]);
+            }
+
+            return dict;
+        }
+
+        /// <summary>
+        /// Get the EnvironmentName whether running local or on an AWS EBS instance.
+        /// In AWS, this would be an environment variable - ASPNETCORE_ENVIRONMENT
+        /// </summary>
+        /// <param name="env"></param>
+        /// <returns>environment name (string) ex. "Development", "Production", etc...</returns>
+        private static string GetEnvironmentName(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            const string awsContainerConfigLocation = @"C:\Program Files\Amazon\ElasticBeanstalk\config\containerconfiguration";
+            const string aspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+            const string configSection = "iis:env";
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile(awsContainerConfigLocation, optional: true, reloadOnChange: true).Build();
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (IConfigurationSection pair in config.GetSection(configSection).GetChildren())
+            {
+                string[] keypair = pair.Value.Split(new[] { '=' }, 2);
+                dict.Add(keypair[0], keypair[1]);
+            }
+
+            return dict.ContainsKey(aspNetCoreEnvironment)
+                ? dict[aspNetCoreEnvironment]
+                : env.EnvironmentName;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
